c14b38ecc809cd0cd263e189830c3831d6f6cef7
Aryaman <pantherdox@gmail.com>
2023-07-19 11:53:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const text = document.querySelector('.sec-text');
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = "Developer";
+    }, 0);
+
+    setTimeout(() => {
+        text.textContent = "Freelancer";
+    }, 4000);
+
+    setTimeout(() => {
+        text.textContent = "YouTuber";
+    }, 8000);
+}
+
+textLoad();
+setInterval(textLoad, 12000);
